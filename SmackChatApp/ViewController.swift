0bda4c481f6258399732aa068177b1083c4b50ae
Abhishek Dantale <abhishekdantale@Abhisheks-MacBook-Pro.local>
2019-01-03 23:29:17 +0530

    Ver0.00

---
+++ SmackChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackChatApp
+//
+//  Created by Abhishek Dantale on 03/01/19.
+//  Copyright © 2019 Abhishek Dantale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
